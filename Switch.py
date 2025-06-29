2059fdf56191cb42b2c30a81ff63b5e723af8809
Dawid Leszczynski <leszczynskidawid3@gmail.com>
2025-06-29 22:05:12 +0200

    important file to upload on board

--- Switch.py
+++ Switch.py
@@ -6,12 +6,24 @@ board = pyfirmata.Arduino('COM5')  # Adjust the port as necessary
 it = pyfirmata.util.Iterator(board)
 it.start()
 
+"""
 board.digital[10].mode = pyfirmata.INPUT  # Set pin 10 as input
 
 while True:
-    sw = board.digital[10].read()  # Read the state of the switch
+    sw = board.digital[gfhfth10].read()  # Read the state of the switch
     if sw is True:
         board.digital[13].write(1) 
     else:
         board.digital[13].write(0)
-    time.sleep(0.1) # Turn on the LED
+    time.sleep(0.1) ### Turn on the LED
+"""
+digital_pin = board.get_pin('d:10:i')  # Set pin 10 as input
+led_pin = board.get_pin('d:13:o')  # Set pin 13 as output
+
+while True:
+    sw = digital_pin.read()  # Read the state of the switch
+    if sw is True:
+        led_pin.write(1)
+    else:
+        led_pin.write(0)
+    time.sleep(0.1)  # Wait for 100 milliseconds
